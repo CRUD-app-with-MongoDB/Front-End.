c0136995cdeb025334ea8293d60a2ffdbbbdcc9c
Luke McCarthy <astartofanewlight@gmail.com>
2023-10-30 04:21:27 -0500

    book com started

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from "axios";
+
+let VITE_APP_SERVER = import.meta.env.VITE_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,14 +12,31 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async () => {
+    console.log(`${VITE_APP_SERVER}/books`);
+      try {
+        let results = await axios.get(`${VITE_APP_SERVER}/books`);
+        console.log('what happened! ',results);
+        this.setState({
+          books: results.data,
+        })
+      } catch (error) {
+        console.log("we have an error: ", error.response.data);
+      }
+    };
 
   render() {
 
     /* TODO: render all the books in a Carousel */
+    console.log(this.state.books);
+    let books = this.state.books.map(book => (
+     <p key={book.title}>{book.description} is {book.available}</p>
+    ))
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+      
+        <h1>My Essential Lifelong Learning &amp; Formation Shelf</h1>
 
         {this.state.books.length ? (
           <p>Book Carousel coming soon</p>
