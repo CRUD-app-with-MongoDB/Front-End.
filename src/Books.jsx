ac1aee2da541575cdb15a33ea6bc44f03b7c5ad8
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-31 12:57:44 -0500

    added update button

--- src/Books.jsx
+++ src/Books.jsx
@@ -4,7 +4,12 @@ import { Container, ListGroup, Button, ListGroupItem} from "react-bootstrap";
 class Books extends React.Component {
     render () {
         let books = this.props.books.map((book) => (
-            <Book book={book} key={book._id} deleteBooks={this.props.deleteBooks} />
+            <Book 
+            book={book} 
+            key={book._id} 
+            deleteBooks={this.props.deleteBooks} 
+            updateBooks={this.props.updateBooks}
+            />
         ));
         return (
             <>
@@ -24,6 +29,7 @@ class Book extends Books {
                 {this.props.book.title} is a book about {this.props.book.description} 
                 
                 <Button variant="success" onClick={() => this.props.deleteBooks(this.props.book._id)}>Delete Book</Button>
+                <Button variant="warning" onClick={() => this.props.updateBooks(this.props.book)}>Update Book</Button>
                 </ListGroupItem></>
         )
     }
