1070be8978900bc3b9a21e60c693fe8109da3714
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-31 02:17:49 -0500

    added carousel

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import axios from "axios";
+import { Carousel } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+// import { buildErrorMessage } from 'vite';
 
 let VITE_APP_SERVER = import.meta.env.VITE_APP_SERVER;
-console.log('server...',VITE_APP_SERVER);
+// console.log('server...', VITE_APP_SERVER);
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -14,44 +17,62 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
   getBooks = async () => {
-    console.log(`${VITE_APP_SERVER}/books`);
-      try {
-        let results = await axios.get(`${VITE_APP_SERVER}/books`);
-        console.log('what happened! ',results);
-        this.setState({
-          books: results.data,
-        })
-      } catch (error) {
-        console.log("we have an error: ", error.response.data);
-      }
-    };
-
-    componentDidMount() {
-      this.getBooks();
+    // console.log(`${VITE_APP_SERVER}/books`);
+    try {
+      let results = await axios.get(`${VITE_APP_SERVER}/books`);
+      // console.log('what happened! ', results);
+      this.setState({
+        books: results.data,
+      })
+    } catch (error) {
+      console.log("we have an error: ", error.response.data);
     }
+  };
+
+  componentDidMount() {
+    this.getBooks();
+  }
 
 
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
-    console.log(this.state.books);
-    let books = this.state.books.map(book => (
-     <p key={book.title}>{book.description} is {book.available}</p>
+    let carouselItems = this.state.books.map((book, index) => (
+      <Carousel.Item key={index}>
+        <img className="d-block w-100" alt={this.state.books.description} />
+        {/* <Carousel.Caption> */}
+          <h3
+            style={{
+              backgroundColor: "black",
+              fontSize: "24px",
+              borderRadius: "15px",
+              width: "max-content",
+              margin: "auto",
+              padding: "5px"
+            }}
+          >{book.title}</h3>
+        {/* </Carousel.Caption> */}
+      </Carousel.Item>
     ))
 
     return (
       <>
-      
         <h1>My Essential Lifelong Learning &amp; Formation Shelf</h1>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon {books}</p>
+          <><Container>
+            <Carousel style={{
+              height: "80px",
+              color: "red",
+              backgroundColor: "maroon",
+            }}> 
+              {carouselItems}
+            </Carousel>
+          </Container></>
         ) : (
           <h3>No Books Found :(</h3>
         )}
       </>
-    )
+    );
   }
 }
 
